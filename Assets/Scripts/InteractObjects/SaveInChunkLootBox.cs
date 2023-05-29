9e8031b1cc19e7dd3ec5364457772d54eb3e2c91
zaletovfyodor <zaletovfyodor@gmail.com>
2023-05-29 17:53:32 +0300

    Added art

---
+++ Assets/Scripts/InteractObjects/SaveInChunkLootBox.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SaveInChunk : MonoBehaviour
+{
+    [SerializeField] private InteractLootBox _interactLootBox;
+
+    private int _index;
+
+    public void SetIndex(int index) => _index = index;
+
+    public int GetIndex() => _index;
+    
+    private void UpdateFields()
+    {
+        if ((_interactLootBox == null || _interactLootBox == default))
+            _interactLootBox = GetComponent<InteractLootBox>();
+    }
+}
